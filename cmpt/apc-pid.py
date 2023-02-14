7d6c4e8ea8a45f2926eed2414006a53fcfb14dca
I-Ieta0 <1692880124@qq.com>
2023-02-14 15:11:03 +0800

    python 自定义组件微服务功能实现

---
+++ cmpt/apc-pid.py
@@ -0,0 +1,19 @@
+#import start
+import numpy #numpy
+from selenium import webdriver  #selenium
+
+### customer code start
+def main(input_data, context):
+    IN1 = input_data["IN1"] or None
+    a = numpy.array([[1,2,3],[4,5,6]]) 
+    print(a)
+
+    OUT1 = {
+      "dataKey": {
+        "value": 0,
+        "keyName": "keyName",
+        "quality": 192
+      }
+    }
+    return OUT1
+### customer code end
